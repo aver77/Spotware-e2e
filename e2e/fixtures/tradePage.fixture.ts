e677a1e91b54e3f62c948ca0094e860cf922def9
Nikita Averochkin <dota.nikita@inbox.ru>
2024-04-27 17:24:59 +0300

    :construction: added fixture, renamed files

---
+++ e2e/fixtures/tradePage.fixture.ts
@@ -0,0 +1,12 @@
+import { test as base } from "@playwright/test";
+import TradePage from "pages/tradePage.page";
+
+export const test = base.extend<{
+    tradePage: TradePage;
+}>({
+    tradePage: async ({ page }, use) => {
+        const tradePage = new TradePage(page);
+        await tradePage.page.goto("https://app.ctrader.com");
+        await use(tradePage);
+    }
+});
